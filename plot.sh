81a6a8943a53cb303839a7e095a1162fbfb26ffe
Jonathan Lee <123abcjonathanlee@gmail.com>
2017-10-06 13:21:26 -0700

    rearranged files for ubuntu

--- plot.sh
+++ plot.sh
@@ -1,11 +1,11 @@
-python plotting/plot_loss.py --env Hopper-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
-python plotting/plot_reward.py --env Hopper-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
+python plot_loss.py --env Hopper-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
+python plot_reward.py --env Hopper-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
 
-python plotting/plot_loss.py --env Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
-python plotting/plot_reward.py --env Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
+python plot_loss.py --env Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
+python plot_reward.py --env Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
 
-python plotting/plot_loss.py --env HalfCheetah-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
-python plotting/plot_reward.py --env HalfCheetah-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
+python plot_loss.py --env HalfCheetah-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
+python plot_reward.py --env HalfCheetah-v1 --t 500 --iters 5 10 15 20 --update 1 7 --save
 
-python plotting/plot_loss.py --env Humanoid-v1 --t 500 --iters 50 100 150 200 --update 60 75 90 105 120 135 150 165 180 195 --save
-python plotting/plot_reward.py --env Humanoid-v1 --t 500 --iters 50 100 150 200 --update 60 75 90 105 120 135 150 165 180 195 --save
+python plot_loss.py --env Humanoid-v1 --t 500 --iters 50 100 150 200 --update 60 75 90 105 120 135 150 165 180 195 --save
+python plot_reward.py --env Humanoid-v1 --t 500 --iters 50 100 150 200 --update 60 75 90 105 120 135 150 165 180 195 --save
--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
-python tests/bc.py --envname Hopper-v1 --t 500 --iters 5 10 15 20
-python tests/dart.py --envname Hopper-v1 --t 500 --iters 5 10 15 20 --update 1 7
-python tests/rand.py --envname Hopper-v1 --t 500 --iters 5 10 15 20 --prior 1.0
-python tests/dagger.py --envname Hopper-v1 --t 500 --iters 5 10 15 20 --beta .5
+python test_bc.py --envname Hopper-v1 --t 500 --iters 5 10 15 20
+python test_dart.py --envname Hopper-v1 --t 500 --iters 5 10 15 20 --update 1 7
+python test_rand.py --envname Hopper-v1 --t 500 --iters 5 10 15 20 --prior 1.0
+python test_dagger.py --envname Hopper-v1 --t 500 --iters 5 10 15 20 --beta .5
 
-# python test_bc.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20
-# python test_dart.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7
-# python test_rand.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --prior 1.0
+python test_bc.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20
+python test_dart.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7
+python test_rand.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --prior 1.0
+python test_dagger.py --envname Hopper-v1 --t 500 --iters 5 10 15 20 --beta .5
 
-# python test_bc.py --envname HalfCheetah-v1 --t 500 --iters 5 10 15 20
-# python test_dart.py --envname HalfCheetah-v1 --t 500 --iters 5 10 15 20 --update 1 7
-# python test_rand.py --envname HalfCheetah-v1 --t 500 --iters 5 10 15 20 --prior 1.0
+python test_bc.py --envname HalfCheetah-v1 --t 500 --iters 5 10 15 20
+python test_dart.py --envname HalfCheetah-v1 --t 500 --iters 5 10 15 20 --update 1 7
+python test_rand.py --envname HalfCheetah-v1 --t 500 --iters 5 10 15 20 --prior 1.0
+python test_dagger.py --envname -v1 --t 500 --iters 5 10 15 20 --beta .5
