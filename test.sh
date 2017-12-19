aa4a601d281b6f4e91e30e6cd3e10f2191c67406
xu.tao <xu.tao@slxrobot.com>
2017-12-19 10:19:33 +0800

    corrections for test.sh

--- test.sh
+++ test.sh
@@ -30,11 +30,11 @@ python experiments/test_rand.py --envname HalfCheetah-v1 --t 500 --iters 5 10 15
 
 python experiments/test_bc.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200
 python experiments/test_dart.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --update 60 75 90 105 120 135 150 165 180 195
-python experiments/test_dagger --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --beta .5
+python experiments/test_dagger.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --beta .5
 python experiments/test_dagger_b.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --update 60 75 90 105 120 135 150 165 180 195 --beta .5
 python experiments/test_iso.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --scale 1.0
 
 python experiments/test_rand.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --trace 0.005
-python experiments/test_rand.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --trace 0.05
+python experiments/test_rand.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --trace 0.5
 python experiments/test_rand.py --envname Humanoid-v1 --t 500 --iters 50 100 150 200 --trace 10.0
 
