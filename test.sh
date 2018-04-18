df374483d940a01d1541ffcf4499148a15a2af8d
Michael Laskey <mdlaskey@umich.edu>
2018-04-18 15:30:13 -0700

    added new installation script

--- test.sh
+++ test.sh
@@ -29,10 +29,10 @@
 # python experiments/test_dart.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7
 # python experiments/test_dart3.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 10
 # python experiments/test_dart3.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 3
-python experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 5
-python experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 10
-python experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 50
-python experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 450
+python2 experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 5
+python2 experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 10
+python2 experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 50
+python2 experiments/test_dart2.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 450
 # python experiments/test_dart3.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --partition 3
 # python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --beta .5
 # python experiments/test_dagger_b.py --envname Walker2d-v1 --t 500 --iters 5 10 15 20 --update 1 7 --beta .1
